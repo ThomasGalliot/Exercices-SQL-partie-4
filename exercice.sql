66474f5c6baa5be4b8263fd8513797ec5f5a1212
ThomasGalliot <thomas.galliot@hotmail.fr>
2018-07-24 14:51:58 +0200

    azerty

---
+++ exercice.sql
@@ -0,0 +1,17 @@
+--exercice1
+INSERT INTO webDeveloppement.languages
+    VALUES (1, 'JavaScript', 'version 5'),
+    (NULL, 'PHP', 'version 5.2'),
+    (NULL, 'PHP', 'version 5.4'),
+    (NULL, 'HTML', 'version 5.1'),
+    (NULL, 'JavaScript', 'version 6'),
+    (NULL, 'JavaScript', 'version 7'),
+    (NULL, 'JavaScript', 'version 8'),
+    (NULL, 'PHP', 'version 7');
+
+--exercice2
+INSERT INTO webDeveloppement.frameworks
+    VALUES (1, 'Symfony', 'version 2.8'),
+    (NULL, 'Symfony', 'version 3'),
+    (NULL, 'Jquery', 'version 1.6'),
+    (NULL, 'Jquery', 'version 2.10');
